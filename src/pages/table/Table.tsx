322e116e24b81c74b37c8c6d858acbb64382f72b
Sai Min Pyae Kyaw <saiminpyaekyaw@gmail.com>
2024-09-15 22:19:25 +0630

    feat(table): added table

--- src/pages/table/Table.tsx
+++ src/pages/table/Table.tsx
@@ -1,3 +1,11 @@
+import { DataTable } from "./components/data-table";
+import { columns } from "./components/columns";
+import { tasks } from "./data/tasks";
+
 export function Table() {
-  return <h1>Table</h1>;
+  return (
+    <div>
+      <DataTable data={tasks} columns={columns} />
+    </div>
+  );
 }
