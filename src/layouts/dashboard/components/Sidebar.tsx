1c2c73fd8c58b127e5c741c90a2b7d7b2071033c
Sai Min Pyae Kyaw <saiminpyaekyaw@gmail.com>
2024-09-28 03:43:09 +0630

    refactor(dashboard-layout): refactor sidebar ui

--- src/layouts/dashboard/components/Sidebar.tsx
+++ src/layouts/dashboard/components/Sidebar.tsx
@@ -10,15 +10,38 @@ export function Sidebar() {
   const { sidebarCollapsed, toggleSidebar } = useUserPreference();
 
   return (
-    <aside
-      className={clsx(
-        "transition-all duration-300 fixed inset-y-0 left-0 z-10 hidden justify-center flex-col border-r bg-background sm:flex",
-        sidebarCollapsed ? "w-[60px]" : "w-[250px]"
-      )}
-    >
+    <>
+      <aside
+        className={clsx(
+          "transition-all duration-300 overflow-hidden fixed inset-y-0 left-0 z-10 hidden justify-center flex-col border-r bg-background sm:flex",
+          sidebarCollapsed ? "w-[60px]" : "w-[250px]"
+        )}
+      >
+        <nav className={clsx("flex flex-col gap-1 p-2 items-start")}>
+          <SidebarHeader />
+
+          {menus.map((m) => (
+            <SidebarItem
+              key={m.route}
+              icon={m.icon}
+              name={m.name}
+              route={m.route}
+              childs={m.childs}
+            />
+          ))}
+        </nav>
+
+        <nav className="mt-auto flex flex-col items-start gap-4 px-2">
+          <SidebarFooter />
+        </nav>
+      </aside>
+
       <div
         onClick={toggleSidebar}
-        className="absolute right-[-11px] h-[22px] w-[22px] cursor-pointer bg-primary flex items-center justify-center rounded-full text-primary-foreground"
+        className={clsx(
+          "transition-all duration-300 fixed z-20 top-[50%] h-[22px] w-[22px] cursor-pointer bg-primary hidden sm:flex items-center justify-center rounded-full text-primary-foreground",
+          sidebarCollapsed ? "left-[48px]" : "left-[238px]"
+        )}
       >
         {sidebarCollapsed ? (
           <ChevronRight className="h-[18px] w-[18px]" />
@@ -26,24 +49,6 @@ export function Sidebar() {
           <ChevronLeft className="h-[18px] w-[18px]" />
         )}
       </div>
-
-      <nav className={clsx("flex flex-col gap-1 p-2 items-start")}>
-        <SidebarHeader />
-
-        {menus.map((m) => (
-          <SidebarItem
-            key={m.route}
-            icon={m.icon}
-            name={m.name}
-            route={m.route}
-            childs={m.childs}
-          />
-        ))}
-      </nav>
-
-      <nav className="mt-auto flex flex-col items-start gap-4 px-2">
-        <SidebarFooter />
-      </nav>
-    </aside>
+    </>
   );
 }
--- src/pages/charts/charts-page.tsx
+++ src/pages/charts/charts-page.tsx
@@ -8,63 +8,63 @@ import AreaChartLinear from "@components/charts/chart-area-linear";
 import AreaChartStacked from "@components/charts/chart-area-stacked";
 import AreaChartStackedExpand from "@components/charts/chart-area-stacked-expand";
 import AreaChartStep from "@components/charts/chart-area-step";
-import BarChartActive from "@components/charts/chart-bar-active";
-import BarChartDefault from "@components/charts/chart-bar-default";
-import BarChartLabelCustom from "@components/charts/chart-bar-label-custom";
-import BarChartLabel from "@components/charts/chart-bar-label";
-import BarChartHorizontal from "@components/charts/chart-bar-horizontal";
-import BarChartMixed from "@components/charts/chart-bar-mixed";
-import BarChartMultiple from "@components/charts/chart-bar-multiple";
-import BarChartNegative from "@components/charts/chart-bar-negative";
-import BarChartStacked from "@components/charts/chart-bar-stacked";
-import LineChartDefault from "@components/charts/chart-line-default";
-import LineChartDotsColors from "@components/charts/chart-line-dots-colors";
-import LineChartDotsCustom from "@components/charts/chart-line-dots-custom";
-import LineChartDots from "@components/charts/chart-line-dots";
-import LineChartLabelCustom from "@components/charts/chart-line-label-custom";
-import LineChartLabel from "@components/charts/chart-line-label";
-import LineChartLinear from "@components/charts/chart-line-linear";
-import LineChartMultiple from "@components/charts/chart-line-multiple";
-import LineChartStep from "@components/charts/chart-line-step";
-import PieChartDonutActive from "@components/charts/chart-pie-donut-active";
-import PieChartDonutText from "@components/charts/chart-pie-donut-text";
-import PieChartDonut from "@components/charts/chart-pie-donut";
-import PieChartInteractive from "@components/charts/chart-pie-interactive";
-import PieChartLabelCustom from "@components/charts/chart-pie-label-custom";
-import PieChartLabelList from "@components/charts/chart-pie-label-list";
-import PieChartLabel from "@components/charts/chart-pie-label";
-import PieChartLegend from "@components/charts/chart-pie-legend";
-import PieChartSeparatorNone from "@components/charts/chart-pie-separator-none";
-import PieChartSimple from "@components/charts/chart-pie-simple";
-import PieChartStacked from "@components/charts/chart-pie-stacked";
-import RadarChartDefault from "@components/charts/chart-radar-default";
-import RadarChartDots from "@components/charts/chart-radar-dots";
-import RadarChartGridCircleFill from "@components/charts/chart-radar-grid-circle-fill";
-import RadarChartGridCircleNoLines from "@components/charts/chart-radar-grid-circle-no-lines";
-import RadarChartGridCircle from "@components/charts/chart-radar-grid-circle";
-import RadarChartGridCustom from "@components/charts/chart-radar-grid-custom";
-import RadarChartGridFill from "@components/charts/chart-radar-grid-fill";
-import RadarChartGridNone from "@components/charts/chart-radar-grid-none";
-import RadarChartIcon from "@components/charts/chart-radar-icons";
-import RadarChartLabelCustom from "@components/charts/chart-radar-label-custom";
-import RadarChartLegend from "@components/charts/chart-radar-legend";
-import RadarChartLinesOnly from "@components/charts/chart-radar-lines-only";
-import RadarChartMultiple from "@components/charts/chart-radar-multiple";
-import RadialChartGrid from "@components/charts/chart-radial-grid";
-import RadialChartLabel from "@components/charts/chart-radial-label";
-import RadialChartShape from "@components/charts/chart-radial-shape";
-import RadialChartSimple from "@components/charts/chart-radial-simple";
-import RadialChartStacked from "@components/charts/chart-radial-stacked";
-import RadialChartText from "@components/charts/chart-radial-text";
-import TooltipChartAdvanced from "@components/charts/chart-tooltip-advanced";
-import TooltipChartDefault from "@components/charts/chart-tooltip-default";
-import TooltipChartFormatter from "@components/charts/chart-tooltip-formatter";
-import TooltipChartIcon from "@components/charts/chart-tooltip-icons";
-import TooltipChartIndicatorLine from "@components/charts/chart-tooltip-indicator-line";
-import TooltipChartIndicatorNone from "@components/charts/chart-tooltip-indicator-none";
-import TooltipChartLabelCustom from "@components/charts/chart-tooltip-label-custom";
-import TooltipChartLabelFormatter from "@components/charts/chart-tooltip-label-formatter";
-import TooltipChartLabelNone from "@components/charts/chart-tooltip-label-none";
+// import BarChartActive from "@components/charts/chart-bar-active";
+// import BarChartDefault from "@components/charts/chart-bar-default";
+// import BarChartLabelCustom from "@components/charts/chart-bar-label-custom";
+// import BarChartLabel from "@components/charts/chart-bar-label";
+// import BarChartHorizontal from "@components/charts/chart-bar-horizontal";
+// import BarChartMixed from "@components/charts/chart-bar-mixed";
+// import BarChartMultiple from "@components/charts/chart-bar-multiple";
+// import BarChartNegative from "@components/charts/chart-bar-negative";
+// import BarChartStacked from "@components/charts/chart-bar-stacked";
+// import LineChartDefault from "@components/charts/chart-line-default";
+// import LineChartDotsColors from "@components/charts/chart-line-dots-colors";
+// import LineChartDotsCustom from "@components/charts/chart-line-dots-custom";
+// import LineChartDots from "@components/charts/chart-line-dots";
+// import LineChartLabelCustom from "@components/charts/chart-line-label-custom";
+// import LineChartLabel from "@components/charts/chart-line-label";
+// import LineChartLinear from "@components/charts/chart-line-linear";
+// import LineChartMultiple from "@components/charts/chart-line-multiple";
+// import LineChartStep from "@components/charts/chart-line-step";
+// import PieChartDonutActive from "@components/charts/chart-pie-donut-active";
+// import PieChartDonutText from "@components/charts/chart-pie-donut-text";
+// import PieChartDonut from "@components/charts/chart-pie-donut";
+// import PieChartInteractive from "@components/charts/chart-pie-interactive";
+// import PieChartLabelCustom from "@components/charts/chart-pie-label-custom";
+// import PieChartLabelList from "@components/charts/chart-pie-label-list";
+// import PieChartLabel from "@components/charts/chart-pie-label";
+// import PieChartLegend from "@components/charts/chart-pie-legend";
+// import PieChartSeparatorNone from "@components/charts/chart-pie-separator-none";
+// import PieChartSimple from "@components/charts/chart-pie-simple";
+// import PieChartStacked from "@components/charts/chart-pie-stacked";
+// import RadarChartDefault from "@components/charts/chart-radar-default";
+// import RadarChartDots from "@components/charts/chart-radar-dots";
+// import RadarChartGridCircleFill from "@components/charts/chart-radar-grid-circle-fill";
+// import RadarChartGridCircleNoLines from "@components/charts/chart-radar-grid-circle-no-lines";
+// import RadarChartGridCircle from "@components/charts/chart-radar-grid-circle";
+// import RadarChartGridCustom from "@components/charts/chart-radar-grid-custom";
+// import RadarChartGridFill from "@components/charts/chart-radar-grid-fill";
+// import RadarChartGridNone from "@components/charts/chart-radar-grid-none";
+// import RadarChartIcon from "@components/charts/chart-radar-icons";
+// import RadarChartLabelCustom from "@components/charts/chart-radar-label-custom";
+// import RadarChartLegend from "@components/charts/chart-radar-legend";
+// import RadarChartLinesOnly from "@components/charts/chart-radar-lines-only";
+// import RadarChartMultiple from "@components/charts/chart-radar-multiple";
+// import RadialChartGrid from "@components/charts/chart-radial-grid";
+// import RadialChartLabel from "@components/charts/chart-radial-label";
+// import RadialChartShape from "@components/charts/chart-radial-shape";
+// import RadialChartSimple from "@components/charts/chart-radial-simple";
+// import RadialChartStacked from "@components/charts/chart-radial-stacked";
+// import RadialChartText from "@components/charts/chart-radial-text";
+// import TooltipChartAdvanced from "@components/charts/chart-tooltip-advanced";
+// import TooltipChartDefault from "@components/charts/chart-tooltip-default";
+// import TooltipChartFormatter from "@components/charts/chart-tooltip-formatter";
+// import TooltipChartIcon from "@components/charts/chart-tooltip-icons";
+// import TooltipChartIndicatorLine from "@components/charts/chart-tooltip-indicator-line";
+// import TooltipChartIndicatorNone from "@components/charts/chart-tooltip-indicator-none";
+// import TooltipChartLabelCustom from "@components/charts/chart-tooltip-label-custom";
+// import TooltipChartLabelFormatter from "@components/charts/chart-tooltip-label-formatter";
+// import TooltipChartLabelNone from "@components/charts/chart-tooltip-label-none";
 
 export function Charts() {
   return (
@@ -80,7 +80,7 @@ export function Charts() {
       <AreaChartStacked />
       <AreaChartStep />
 
-      <BarChartActive />
+      {/* <BarChartActive />
       <BarChartDefault />
       <BarChartHorizontal />
       <BarChartLabelCustom />
@@ -141,7 +141,7 @@ export function Charts() {
       <TooltipChartIndicatorNone />
       <TooltipChartLabelCustom />
       <TooltipChartLabelFormatter />
-      <TooltipChartLabelNone />
+      <TooltipChartLabelNone /> */}
     </div>
   );
 }
