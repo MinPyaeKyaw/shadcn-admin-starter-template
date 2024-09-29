51af84f9584524d9ae7a2411a7f721cbbabf8089
Sai Min Pyae Kyaw <saiminpyaekyaw@gmail.com>
2024-09-29 13:04:44 +0630

    remove(sonnar): vars

--- src/configs/vars.tsx
+++ src/configs/vars.tsx
@@ -5,30 +5,3 @@ export const ADMIN = {
   username: "admin",
   password: "password",
 };
-
-export const SONNER = {
-  SUCCESS: {
-    style: {
-      backgroundColor: "green",
-      color: "white",
-    },
-  },
-  ERROR: {
-    style: {
-      backgroundColor: "red",
-      color: "white",
-    },
-  },
-  INFO: {
-    style: {
-      backgroundColor: "blue",
-      color: "white",
-    },
-  },
-  WARNING: {
-    style: {
-      backgroundColor: "orange",
-      color: "white",
-    },
-  },
-};
