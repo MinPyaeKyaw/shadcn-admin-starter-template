39cd96c2abf50263f823318c22b024fa678342c1
Sai Min Pyae Kyaw <saiminpyaekyaw@gmail.com>
2024-10-03 04:12:16 +0630

    refactor(table): move data table to components

--- src/pages/table/components/columns.tsx
+++ src/pages/table/components/columns.tsx
@@ -1,6 +1,6 @@
 import { Checkbox } from "@components/ui/checkbox";
-import { DataTableColumnHeader } from "./data-table-column-header";
-import { DataTableRowActions } from "./data-table-row-actions";
+import { DataTableColumnHeader } from "@components/data-table/data-table-column-header";
+import { DataTableRowActions } from "@components/data-table/data-table-row-actions";
 
 export const columns: any = [
   {
--- src/pages/table/table-page.tsx
+++ src/pages/table/table-page.tsx
@@ -1,4 +1,4 @@
-import { DataTable } from "./components/data-table";
+import { DataTable } from "@components/data-table/data-table";
 import { columns } from "./components/columns";
 import PageTitle from "@components/commons/page-title";
 import { useState } from "react";
